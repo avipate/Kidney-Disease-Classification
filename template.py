2a2c7a6f00332abb46bf10f8c74a969d2d35046e
Avi Patel <avip1607@outlook.com>
2023-11-29 17:06:35 +0530

    Add: template.py file

---
+++ template.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+import logging
+
+# Logging string
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(messagec c)s:')
+
+project_name = 'cnnClassifier'
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.py",
+    "templates/index.html"
+]
