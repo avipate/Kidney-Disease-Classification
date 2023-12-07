12d1f36af1f12ea2f59ceced76072a9ac8a198a7
Avi Patel <avip1607@outlook.com>
2023-12-07 11:40:22 +0530

    Add: prepased base model configuration

---
+++ research/02_prepare_base_model.py
@@ -0,0 +1,121 @@
+from dataclasses import dataclass
+import os
+import keras.applications.vgg16
+
+from src.cnnClassifier.constants import *
+from src.cnnClassifier.utils.common import read_yaml, create_directories
+import urllib.request as request
+from zipfile import ZipFile
+from keras.layers import Flatten, Dense
+from keras.models import Model
+from keras.optimizers import SGD
+from keras.losses import CategoricalCrossentropy
+
+os.chdir("../")
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+
+# Base Model Configuration class
+class ConfigurationManager:
+    def __init__(self,
+                 config_filepath=CONFIG_FILE_PATH,
+                 params_filepath=PARAMS_FILE_PATH):
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES,
+        )
+
+        return prepare_base_model_config
+
+
+class PrepareBaseModel:
+    def __init__(self, config: PrepareBaseModelConfig):
+        self.config = config
+
+    def get_base_model(self):
+        self.model = keras.applications.vgg16.VGG16(
+            input_shape=self.config.params_image_size,
+            weights=self.config.params_weights,
+            include_top=self.config.params_include_top
+        )
+
+        self.save_model(path=self.config.base_model_path, model=self.model)
+
+    @staticmethod
+    def _prepare_fill_model(model, classes, freeze_all, freeze_till, learning_rate):
+        if freeze_all:
+            for layer in model.layers:
+                model.trainable = False
+        elif (freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till]:
+                model.trainable = False
+
+        flatten_in = Flatten()(model.output)
+        prediction = Dense(
+            units=classes,
+            activation="softmax"
+        )(flatten_in)
+
+        full_model = Model(
+            input_shape=model.input,
+            output=prediction
+        )
+
+        full_model.compile(
+            optimizer=SGD(learning_rate=learning_rate),
+            loss=CategoricalCrossentropy(),
+            metrics=["accuracy"]
+        )
+
+        full_model.summary()
+        return full_model
+
+    def update_base_model(self):
+        self.full_model = self._prepare_fill_model(
+            model=self.model,
+            classes=self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
+            learning_rate=self.config.params_learning_rate
+        )
+
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+
+    @staticmethod
+    def save_model(path: Path, model: Model):
+        model.save(path)
+
+
+try:
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+except Exception as e:
+    raise e
