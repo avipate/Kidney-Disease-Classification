9e953c7b797f4fdab1cc6691a915c7727b5bef41
Avi Patel <avip1607@outlook.com>
2023-12-11 18:57:48 +0530

    Add: get evaluation config method

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,9 @@
 import os
+
 from src.cnnClassifier.constants import *
-from src.cnnClassifier.utils.common import read_yaml, create_directories, get_size
-from src.cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from src.cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig,
+                                                    EvaluationConfig)
+from src.cnnClassifier.utils.common import read_yaml, create_directories
 
 
 class ConfigurationManager:
@@ -65,3 +67,15 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/avip1607/Kidney-Disease-Classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+    
