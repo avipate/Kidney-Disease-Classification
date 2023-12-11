454f31407b41f371df05e0447ffe86dc57df5c86
Avi Patel <avip1607@outlook.com>
2023-12-11 19:00:32 +0530

    Add: Evaluation paramerets and method to log into mlflow

---
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -0,0 +1,69 @@
+from urllib.parse import urlparse
+
+import keras
+from src.cnnClassifier.entity.config_entity import EvaluationConfig
+from pathlib import Path
+from src.cnnClassifier.utils.common import save_json
+import mlflow.keras
+
+
+class Evaluation:
+    def __init__(self, config: EvaluationConfig):
+        self.config = config
+
+    def _valid_generator(self):
+        data_generator_kwargs = dict(
+            rescale=1. / 255,
+            validation_split=0.30
+        )
+
+        data_flow_kwargs = dict(
+            target_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            interpolation="bilinear"
+        )
+
+        valid_data_generator = keras.preprocessing.image.ImageDataGenerator(
+            **data_generator_kwargs
+        )
+
+        self.valid_generator = valid_data_generator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="validation",
+            shuffle=False,
+            **data_flow_kwargs
+        )
+
+    @staticmethod
+    def load_model(path: Path) -> keras.Model:
+        return keras.models.load_model(path)
+
+    def evaluation(self):
+        self.model = self.load_model(self.config.path_of_model)
+        self._valid_generator()
+        self.score = self.model.evaluate(self.valid_generator)
+        self.save_score()
+
+    def save_score(self):
+        scores = {"loss": self.score[0], "accuracy": self.score[1]}
+        save_json(path=Path("score.json"), data=scores)
+
+    def log_into_mlflow(self):
+        mlflow.set_tracking_uri(self.config.mlflow_uri)
+        tracking_url_type_score = urlparse(mlflow.get_tracking_uri()).scheme
+
+        with mlflow.start_run():
+            mlflow.log_params(self.config.all_params)
+            mlflow.log_metrics(
+                {"loss": self.score[0], "accuracy": self.score[1]}
+            )
+
+            # Model registry does not work with file store
+            if tracking_url_type_score != "file":
+
+                # Register the model
+                # There aare other way to use the Model Registry, which depends on the use case,
+                # Please refer to the doc for more information:
+                mlflow.keras.log_model(self.model, "model", registered_model_name="V16Model")
+            else:
+                mlflow.keras.log_model(self.model, "model")
